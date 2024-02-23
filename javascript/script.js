7a3964dc955d36eb4ba8155ba1890c8a7ea350ee
Marcos <marcos.luan.752@gmail.com>
2024-02-22 22:57:13 -0400

    Projeto Finalizado

--- javascript/script.js
+++ javascript/script.js
@@ -3,20 +3,67 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+const scoreHidden = document.querySelector('.score');
+const score = document.querySelector('.score--value');
+const finalScore = document.querySelector('.final-score > span');
+const menu = document.querySelector('.menu-screen');
+const buttonPlay = document.querySelector('.btn-play');
+
+const audio = new Audio('../assets/audio.mp3');
+
 // Definindo o temanho da cobrinha
 const size = 30;
 
-// Definindo a lógica que dita o tamanho e o movimento da cobrinha
+// Definindo a posição inicial
+const initialPosition = { x: 270, y: 240 };
 
-const snake = [
-	{ x: 200, y: 200 },
-	{ x: 230, y: 200 },
-];
+let snake = [initialPosition];
+
+const incrementScore = () => {
+	score.innerText = +score.innerText + 10;
+};
+
+// Desenhando a comida em posições aleatórias
+const randomNumber = (min, max) => {
+	return Math.round(Math.random() * (max - min) + min);
+};
+
+const randomPosition = () => {
+	const number = randomNumber(0, canvas.width - size);
+	return Math.round(number / size) * size;
+};
+
+// Setando uma cor aleatória para a Food
+
+const randomColorFood = () => {
+	const red = randomNumber(0, 255);
+	const green = randomNumber(0, 255);
+	const blue = randomNumber(0, 255);
+
+	return `rgb(${red}, ${green}, ${blue})`;
+};
+
+const food = {
+	x: randomPosition(),
+	y: randomPosition(),
+	color: randomColorFood(),
+};
+
+// Setando as características da comida
+const drawFood = () => {
+	const { x, y, color } = food;
+
+	ctx.shadowColor = color;
+	ctx.shadowBlur = 8;
+	ctx.fillStyle = color;
+	ctx.fillRect(x, y, size, size);
+	ctx.shadowBlur = 0;
+};
 
 // definindo a direção da cobrinha
 let direction, loopId;
 
-// Atribuindo movimento à ela
+// Atribuindo movimento à cobrinha
 const moveSnake = () => {
 	if (!direction) return;
 
@@ -41,6 +88,45 @@ const moveSnake = () => {
 	snake.shift();
 };
 
+const drawGrid = () => {
+	ctx.lineWidth = 1;
+	ctx.strokeStyle = '#191919';
+
+	for (let i = size; i < canvas.width; i += size) {
+		ctx.beginPath();
+		ctx.lineTo(i, 0);
+		ctx.lineTo(i, 600);
+		ctx.stroke();
+
+		ctx.beginPath();
+		ctx.lineTo(0, i);
+		ctx.lineTo(600, i);
+		ctx.stroke();
+	}
+};
+
+// Constante que checka se a cobrianha comeu a comida
+const checkEat = () => {
+	const head = snake[snake.length - 1];
+
+	if (head.x == food.x && head.y == food.y) {
+		incrementScore();
+		snake.push(head);
+		audio.play();
+		let x = randomPosition();
+		let y = randomPosition();
+
+		while (snake.find((position) => position.x == x && position.y == y)) {
+			x = randomPosition();
+			y = randomPosition();
+		}
+
+		food.x = x;
+		food.y = y;
+		food.color = randomColorFood();
+	}
+};
+
 // lógica que percorre e desenha a cobrinha
 const drawSnake = () => {
 	ctx.fillStyle = '#ddd';
@@ -52,15 +138,47 @@ const drawSnake = () => {
 	});
 };
 
+const gameOver = () => {
+	direction = undefined;
+	scoreHidden.style.display = 'none';
+	menu.style.display = 'flex';
+	finalScore.innerText = score.innerText;
+	canvas.style.filter = 'blur(2px)';
+};
+
+// checkando a colisão
+const checkCollision = () => {
+	const head = snake[snake.length - 1];
+	const canvasLimit = canvas.width - size;
+	const neckIndex = snake.length - 2;
+
+	const wallCollision =
+		head.x < 0 ||
+		head.x > canvasLimit ||
+		head.y < 0 ||
+		head.y > canvasLimit;
+
+	const selfCollision = snake.find((position, index) => {
+		return index < neckIndex && position.x == head.x && position.y == head.y;
+	});
+
+	if (wallCollision || selfCollision) {
+		gameOver();
+	}
+};
+
 // loop responsavel por desenhar a cobrinha e atualizar a tela,
 // limpar o loop para evitar erros.
 //limpar o canvas
 const gameLoop = () => {
 	clearInterval(loopId);
 	ctx.clearRect(0, 0, 600, 600);
-
-	moveSnake();
+	drawFood();
+	drawGrid();
 	drawSnake();
+	moveSnake();
+	checkEat();
+	checkCollision();
 
 	loopId = setTimeout(() => {
 		gameLoop();
@@ -69,7 +187,7 @@ const gameLoop = () => {
 
 gameLoop();
 
-// Mapeano a direção usando o teclado
+// Mapeando a direção usando o teclado
 document.addEventListener('keydown', ({ key }) => {
 	if (key == 'ArrowRight' && direction != 'left') {
 		direction = 'right';
@@ -86,4 +204,13 @@ document.addEventListener('keydown', ({ key }) => {
 	if (key == 'ArrowDown' && direction != 'up') {
 		direction = 'down';
 	}
+});
+
+buttonPlay.addEventListener('click', () => {
+	score.innerText = '00';
+	menu.style.display = 'none';
+	canvas.style.filter = 'none';
+	scoreHidden.style.display = 'flex';
+
+	snake = [initialPosition];
 });
